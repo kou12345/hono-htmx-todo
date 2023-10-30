4af343abd2358cee5680d5138fa55864a78dbe41
kou12345 <hi.0412.ko.2002@gmail.com>
2023-10-31 05:36:31 +0900

    wip

--- src/components/itemForm.tsx
+++ src/components/itemForm.tsx
@@ -3,12 +3,9 @@ import { Todo } from "../types";
 export const ItemForm = ({ todo }: { todo: Todo }) => {
   return (
     <form hx-put={`/todo/${todo.id}/edit`} hx-target="this" hx-swap="outerHTML">
-      <input
-        type="text"
-        name="title"
-        value={todo.title}
-        class="flex row items-center justify-between py-1 px-4 my-1 rounded-lg text-lg border bg-gray-100 text-gray-600 mb-2"
-      />
+      <p class="flex row items-center justify-between py-1 px-4 my-1 rounded-lg text-lg border bg-gray-100 text-gray-600 mb-2">
+        <input type="text" name="title" value={todo.title} />
+      </p>
       <div class="flex justify-end">
         <button
           hx-target={`#todo${todo.id}`}
