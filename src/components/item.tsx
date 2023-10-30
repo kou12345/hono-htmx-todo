f5a4cda497b6e40ec46a78179784f7b5fe815b01
kou12345 <hi.0412.ko.2002@gmail.com>
2023-10-31 05:08:16 +0900

    fix button

--- src/components/item.tsx
+++ src/components/item.tsx
@@ -2,13 +2,28 @@ import { Todo } from "../types";
 
 export const Item = ({ todo }: { todo: Todo }) => {
   return (
-    <p
-      hx-delete={`/todo/${todo.id}`}
-      hx-swap="outerHTML"
-      class="flex row items-center justify-between py-1 px-4 my-1 rounded-lg text-lg border bg-gray-100 text-gray-600 mb-2"
-    >
-      {todo.title}
-      <button class="font-medium">Delete</button>
-    </p>
+    <div id={`todo${todo.id}`}>
+      <p class="flex row items-center justify-between py-1 px-4 my-1 rounded-lg text-lg border bg-gray-100 text-gray-600 mb-2">
+        {todo.title}
+      </p>
+      <div class="flex justify-end">
+        <button
+          hx-delete={`/todo/${todo.id}`}
+          hx-target={`#todo${todo.id}`}
+          hx-swap="outerHTML"
+          class="font-medium px-2 text-blue-600"
+        >
+          Update
+        </button>
+        <button
+          hx-delete={`/todo/${todo.id}`}
+          hx-target={`#todo${todo.id}`}
+          hx-swap="outerHTML"
+          class="font-medium px-2 text-red-600"
+        >
+          Delete
+        </button>
+      </div>
+    </div>
   );
 };
