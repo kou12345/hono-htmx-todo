c28370e5a1a3a7717c6cadeab7c856546318dd7c
kou12345 <hi.0412.ko.2002@gmail.com>
2023-10-31 03:37:17 +0900

    add hyperscript

--- src/components/renderer.tsx
+++ src/components/renderer.tsx
@@ -10,6 +10,7 @@ export const renderer = jsxRenderer(({ children }) => {
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <script src="https://unpkg.com/htmx.org@1.3.3"></script>
+        <script src="https://unpkg.com/hyperscript.org@0.9.9"></script>
         <script src="https://cdn.tailwindcss.com"></script>
         <link rel="stylesheet" href="/src/style.css" />
         <title>ToDo</title>
