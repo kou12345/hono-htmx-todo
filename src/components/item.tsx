882585bacf9f82e659216557c35b1f34d4897985
kou12345 <hi.0412.ko.2002@gmail.com>
2023-10-31 10:59:50 +0900

    fix

--- src/components/item.tsx
+++ src/components/item.tsx
@@ -6,7 +6,6 @@ export const Item = ({ todo }: { todo: Todo }) => {
       <p
         type="text"
         name="title"
-        value={todo.title}
         class="flex row items-center justify-between py-1 px-4 my-1 rounded-lg text-lg border bg-gray-100 text-gray-600 mb-2"
       >
         {todo.title}
