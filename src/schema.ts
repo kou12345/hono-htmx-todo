b810aa66fbbba9843588fe1fcb8de114c8b609c9
kou12345 <hi.0412.ko.2002@gmail.com>
2023-10-31 10:21:57 +0900

    generate and push

--- src/schema.ts
+++ src/schema.ts
@@ -1,3 +1,4 @@
+import { sql } from "drizzle-orm";
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const todos = sqliteTable("todos", {
@@ -5,6 +6,6 @@ export const todos = sqliteTable("todos", {
   title: text("title"),
   description: text("description"),
   completed: integer("completed", { mode: "boolean" }),
-  createdAt: integer("createdAt", { mode: "timestamp" }),
-  updatedAt: integer("updatedAt", { mode: "timestamp" }),
+  createdAt: integer("createdAt", { mode: "timestamp" }).default(sql`CURRENT_TIMESTAMP`),
+  updatedAt: integer("updatedAt", { mode: "timestamp" }).default(sql`CURRENT_TIMESTAMP`),
 });
